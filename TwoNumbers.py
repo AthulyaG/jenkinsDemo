65edde6be607a8d5a0d4ae7467a16b1aa4e2c7f9
AthulyaG <102713988+AthulyaG@users.noreply.github.com>
2022-05-20 13:25:04 +0530

    Update TwoNumbers.py

--- TwoNumbers.py
+++ TwoNumbers.py
@@ -1,12 +1,6 @@
-n1 = input("First number: ")
-n2 = input("\nSecond number: ")
-  
-# Adding two numbers
-# User might also enter float numbers
-sum = float(n1) + float(n2)
-  
-
-prod = float(n1) * float(n2)
-
-print("The product is:",prod)
-print("The sum of {0} and {1} is {2}" .format(n1, n2, sum))
+def add_num(a,b):#function for addition
+    sum=a+b;
+    return sum; #return value
+num1=int(input("input the number one: "))#input from user for num1
+num2=int(input("input the number one :"))#input from user for num2
+print("The sum is",add_num(num1,num2))#call te function
