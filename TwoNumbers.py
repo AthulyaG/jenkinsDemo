cea8d9612e3c450ffdde63f05601e386927bda2e
AthulyaG <102713988+AthulyaG@users.noreply.github.com>
2022-05-20 13:11:25 +0530

    Two numbers

---
+++ TwoNumbers.py
@@ -0,0 +1,12 @@
+n1 = input("First number: ")
+n2 = input("\nSecond number: ")
+  
+# Adding two numbers
+# User might also enter float numbers
+sum = float(n1) + float(n2)
+  
+
+prod = float(n1) * float(n2)
+
+print("The product is:",prod)
+print("The sum of {0} and {1} is {2}" .format(n1, n2, sum))
